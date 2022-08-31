aa9ca054581db6613e489d3f79013545fff4c159
Alex Leung <alexleung25@outlook.com>
2022-08-30 17:58:25 -0700

    Added formatting buttons

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,60 @@
 import './styles/index.css'
+import { saveAs } from 'file-saver'
 
 function stickies() {
     const app = document.getElementById('app') as HTMLElement;
 
     //sticky notes list array
-    const stickyNotesList = [
+    const stickyNote = [
         `
-        <div class="container">
-            <button id="save">Save</button>
-            <button id="normal">Normal</button>
-            <button id="bold">Bold</button>
-            <button id="italic">Italic</button>
-            <button id="underline">Underline</button>
-        
-            <div class="stickyContainer">
-                <div id="sticky" contenteditable="true"></div>
-            </div>
+        <div id="buttonsContainer">
+            <button id="save"><img src="src/icons/save.png"></button>
+            <button id="normal"><img src="src/icons/normal.png"></button>
+            <button id="bold"><img src="src/icons/bold.png"></button>
+            <button id="italic"><img src="src/icons/italic.png"></button>
+            <button id="underline"><img src="src/icons/underline.png"></button>
         </div>
-        `,
 
-        `
-        <div class="container">
-            <button id="save">Save</button>
-            <button id="normal">Normal</button>
-            <button id="bold">Bold</button>
-            <button id="italic">Italic</button>
-            <button id="underline">Underline</button>
-
-            <div class="stickyContainer">
-                <div id="sticky" contenteditable="true"></div>
-            </div>
+        <div id="stickyContainer">
+            <div id="sticky" contenteditable="true"></div>
         </div>
         `,
     ];
 
     //insert sticky notes list into dom
-    for(let i = 0; i < stickyNotesList.length; i++) {
-        app.insertAdjacentHTML('beforeend', stickyNotesList[i]);
+    for(let i = 0; i < stickyNote.length; i++) {
+        app.insertAdjacentHTML('beforeend', stickyNote[i]);
     }
 
-    const textArea = document.getElementById('sticky') as HTMLElement;
+    //normal formatting (must select all text and click this in order to reset formatting)
     const normal = document.getElementById('normal') as HTMLElement;
-    const bold = document.getElementById('bold') as HTMLElement;
-    const italic = document.getElementById('italic') as HTMLElement;
-    const underline = document.getElementById('underline') as HTMLElement;
-
-    function doExecCommand(command: string) {
-        document.execCommand(command, false, 'div');
-        textArea.focus();
-    }
-
     normal.addEventListener('click', (): void => {
-        doExecCommand("removeFormat");
+        document.execCommand("removeFormat", false);
     });
 
+    const bold = document.getElementById('bold') as HTMLElement;
     bold.addEventListener('click', (): void => {
-        doExecCommand("bold");
+        document.execCommand("bold", false);
     });
 
+    const italic = document.getElementById('italic') as HTMLElement;
     italic.addEventListener('click', (): void => {
-        doExecCommand("italic");
-   });
+        document.execCommand("italic", false);
+    });
 
+    const underline = document.getElementById('underline') as HTMLElement;
     underline.addEventListener('click', (): void => {
-        doExecCommand("underline");
-   });
+        document.execCommand("underline", false);
+    });
+
+    const save = document.getElementById('save') as HTMLElement;
+    const sticky = document.getElementById('sticky') as HTMLElement;
+
+    //save text from stickies to .txt when save button is clicked
+    save.onclick = (): void => {
+        const blob = new Blob([sticky.textContent as string], { type: "text/plain; charset=utf-8"});
+
+        saveAs(blob, "minsticky.txt");
+    }
 }
 stickies();
